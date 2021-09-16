ef31a247888404935a1a57068289ca260fe30c4c
Bárbara Romero <bqromero@gmail.com>
2021-09-16 18:39:35 -0300

    Criando interação com o usuário

---
+++ src/br/com/zup/NewBranch.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class NewBranch {
+  public static void main(String[] args) {
+    Scanner leitor = new Scanner(System.in);
+
+    System.out.println("Olá, digite o seu nome: ");
+    String nomeDoUsuário = leitor.nextLine();
+  }
+}
